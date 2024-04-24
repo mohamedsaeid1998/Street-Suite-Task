5bd272305ae0020608d9b4966762e928016e3ca0
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-04-24 13:20:55 +0200

    Fix Deploy Proplem

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,24 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { useDispatch } from "react-redux"
-import { AuthenticationApiSlice } from "./Services/Authentication/AuthSlice"
-import { QuizzesApiSlice } from "./Services/Quizzes/QuizzesSlice"
-import { StudentsApiSlice } from "./Services/Students/StudentsSlice"
-import { GroupsApiSlice } from "./Services/Groups/GroupsSlice"
-import { ResultsApiSlice } from "./Services/Results/ResultsSlice"
-import { QuestionsApiSlice } from "./Services/Questions/QuestionsSlice"
+
 
 const store = configureStore({
   reducer: {
-    [AuthenticationApiSlice.reducerPath]: AuthenticationApiSlice.reducer,
-    [QuizzesApiSlice.reducerPath]: QuizzesApiSlice.reducer,
-    [StudentsApiSlice.reducerPath]: StudentsApiSlice.reducer,
-    [GroupsApiSlice.reducerPath]: GroupsApiSlice.reducer,
-    [ResultsApiSlice.reducerPath]: ResultsApiSlice.reducer,
-    [QuestionsApiSlice.reducerPath]: QuestionsApiSlice.reducer,
+    // [AuthenticationApiSlice.reducerPath]: AuthenticationApiSlice.reducer,
+
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false
-  }).concat([AuthenticationApiSlice.middleware, QuizzesApiSlice.middleware, StudentsApiSlice.middleware, GroupsApiSlice.middleware, ResultsApiSlice.middleware,QuestionsApiSlice.middleware]),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+  //   serializableCheck: false
+  // }).concat([AuthenticationApiSlice.middleware]),
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
