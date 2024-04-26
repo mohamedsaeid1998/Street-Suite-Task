fb06c12c72442c2059a142c2c3af3b317fd7bf67
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-04-26 07:15:14 +0200

    Fix Bug

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,7 +6,7 @@ interface IProps {
 const Header = ({handleSearch,searchValue}:IProps) => {
   return <>
     <header className=' flex items-center'>
-      <h1 className=' text-lg xs:text-2xl sm:text-5xl font-bold xs:border-l-4 border-activeColor xs:pl-3 rounded-tl-sm  mr-2 xs:mr-0 rounded-bl-sm'>ALERTS</h1>
+      <h1 className=' text-lg xs:text-2xl sm:text-5xl font-bold sm:border-l-4 border-activeColor sm:pl-3  rounded-tl-sm  mr-2 xs:mr-0 rounded-bl-sm'>ALERTS</h1>
       <div className="flex items-center p-1.5 rounded-md  sm:ml-6 mr-4 sm:px-3 bg-[#313131] flex-1 ">
         <input onChange={handleSearch} value={searchValue} type="search" className="outline-none flex-1 border-none mr-2  bg-transparent pl-1 text-white caret-activeColor placeholder-white " placeholder='Search by Name'  />
         <FaSearch color="#6B6B6B" size={18} className="cursor-pointer me-auto" />
